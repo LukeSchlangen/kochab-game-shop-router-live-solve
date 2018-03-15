9e27261cefe1c85c32ce85f2c6249ac35e5f32e9
Christopher Black <chris@primeacademy.io>
2018-03-15 13:45:42 -0500

    Final lecture code.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,45 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('document ready');
+    $('#submitButton').on('click', submitGame);
+    // Get the games when the page loads
+    getAllGames();
+}
+
+function getAllGames() {
+    $.ajax({
+        type: 'GET',
+        url: '/game'
+    }).done(function(response){
+        appendToDom(response); // The response is our gameCollection array
+    });
+}
+// Put the games on the DOM
+function appendToDom(gameCollection) {
+    $('#gameContent').empty();
+    for(let game of gameCollection) {
+        console.log('GAME:', game);
+        let tr = $('<tr></tr>');
+        tr.append('<td>' + game.name + '</td>');
+        tr.append('<td>' + game.cost + '</td>');
+        $('#gameContent').append(tr);
+    }
+}
+
+function submitGame() {
+    let gameName = $('#gameName').val();
+    let cost = $('#cost').val();
+    let gameToAdd = {name: gameName, cost: cost};
+    $.ajax({
+        type: 'post',
+        data: gameToAdd,
+        url: '/game'
+    }).done(function(response) {
+        // our response from a POST will just be '200' success
+        console.log('SUCCESS!');
+        // Refresh our game list
+        getAllGames();
+    }).fail(function(response) {
+        alert('Something went wrong...');
+    })
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
 let express = require('express');
 let app = express();
 const PORT = 4001;
+let bodyParser = require('body-parser');
+const gameCollection = []; // Contains objects e.g. {name: 'game name', cost: 0.99}
+
+// Configures bodyParser for jQuery
+// MUST BE DONE BEFORE OUR app.post
+app.use(bodyParser.urlencoded({extended:true})); 
 
 // Serve static files
 app.use(express.static('server/public'));
+
+// Send back all the games.
+app.get('/game', (req, res) => {
+    res.send(gameCollection);
+});
+
+// When we want to add a new game. 
+app.post('/game', (req, res) => {
+    console.log(req.body);
+    let gameToAdd = req.body; // body.name & body.cost
+    gameCollection.push(gameToAdd);
+    console.log(gameCollection);
+    res.sendStatus(200);
+});
 
 // Spin up the server
 app.listen(PORT, () => {
