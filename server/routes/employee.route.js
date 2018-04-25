b621c472da579592204cc8adebbc2485b5f13700
Luke Schlangen <lukeschlangen@gmail.com>
2018-04-25 14:19:48 -0500

    employee saving to database

--- server/routes/employee.route.js
+++ server/routes/employee.route.js
@@ -2,20 +2,34 @@
 // const express = require('express');
 // const router = express.Router();
 const router = require('express').Router();
-
-const employeeList = []; // Contains objects e.g. {name: 'game name', cost: 0.99}
+const Employee = require('../models/employee.schema');
 
 // Send back all the games.
 router.get('/', (req, res) => {
-    res.send(employeeList);
+    Employee.find({})
+        .then((databaseResults) => {
+            // good things happened!
+            res.send(databaseResults);
+        })
+        .catch((error) => {
+            console.log('error make employee find', error);
+            res.sendStatus(500);
+        });
 });
 
 // When we want to add a new game. 
 router.post('/', (req, res) => {
     const employeeToAdd = req.body; // body.name & body.cost
     console.log('Employee to add:', employeeToAdd);
-    employeeList.push(employeeToAdd);
-    res.sendStatus(200);
+    Employee.create(employeeToAdd)
+        .then((databaseResults) => {
+            // good things happened!
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('error make employee find', error);
+            res.sendStatus(500);
+        });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const gameRouter = require('./routes/game.route');
 const employeeRouter = require('./routes/employee.route');
 
+const mongoose = require('mongoose');
+
 const app = express();
 // if on heroku, use the provided port || use 4001
 const PORT = process.env.PORT || 4001;
@@ -17,6 +19,17 @@ app.use(express.static('server/public'));
 
 app.use('/employee', employeeRouter);
 app.use('/game', gameRouter);
+
+const databaseUrl = 'mongodb://localhost:27017/game_shop';
+mongoose.connect(databaseUrl);
+
+mongoose.connection.on('connected', () => {
+    console.log('mongoose connected on', databaseUrl);
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log('mongoose connection failed', error);
+});
 
 // Spin up the server
 app.listen(PORT, () => {
